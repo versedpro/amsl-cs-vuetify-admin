8944d6b6971300b1fc76c1f6ce2002b1c9d9410e
taukeer <taukeer.liaqat@gmail.com>
2020-12-09 14:50:17 +0500

    upload functionality added to supplier-product

--- src/views/components/supplierProducts/supplier-product-expandable.vue
+++ src/views/components/supplierProducts/supplier-product-expandable.vue
@@ -1,16 +1,36 @@
 <template>
   <v-card flat tile class="my-0 px-4" color="orange lighten-5">
     <v-card-title>test</v-card-title>
-    <v-card-action>
-      <v-img
-        lazy-src="https://picsum.photos/id/11/10/6"
-        max-width="250"
-        src="https://picsum.photos/id/11/500/300"
-      ></v-img>
+    <v-card-actions>
+      <v-flex>
+        <v-btn @click="openFileDialog" height="fit-content" width="fit-content" class="pa-0">
+          <!-- added two img tags because browser is not letting  -->
+          <!-- change the image src across domains -->
+          <v-img
+            v-if="!uploaded"
+            :src="remoteSrc"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-width="250"
+          ></v-img>
+
+          <v-img
+            v-else
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-width="250"
+            :src="src"
+          ></v-img>
+          <input ref="uploader" @change="preview" class="d-none" type="file" accept="image/*" />
+        </v-btn>
+        <div>
+          <p class="pt-2 red--text" v-if="invalidAspect">Aspect ratio is not correct</p>
+        </div>
+        <v-btn class="align-self-center mt-4" @click="upload" :disabled="invalidAspect">
+          Upload
+        </v-btn>
+      </v-flex>
       <v-spacer></v-spacer>
-      <v-text-area label="Remarks"></v-text-area>
-    </v-card-action>
-    <v-card-subtitle>ggg</v-card-subtitle>
+      <v-text-field label="Remarks"></v-text-field>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -34,7 +54,7 @@ export default defineComponent({
     const uploaded = ref(false);
     const invalidAspect = ref(false);
     watchEffect(() => {
-      remoteSrc.value = `https://referlo.azurewebsites.net/SupplierProduct/${props.id}/Image`;
+      remoteSrc.value = `${process.env.VUE_APP_API_URL}/SupplierProduct/${props.id}/Image`;
     });
 
     function preview(e) {
