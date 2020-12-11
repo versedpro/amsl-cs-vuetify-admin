85a85fc7987171147908e2864de77edc56e1ae4d
taukeer <taukeer.liaqat@gmail.com>
2020-12-11 19:33:12 +0500

    tui added on privacy and terms page

--- src/views/components/more/privacy.vue
+++ src/views/components/more/privacy.vue
@@ -3,20 +3,56 @@
     <v-card-title class="primary justify-center display-1 text-h5 white--text">
       {{ title }}
     </v-card-title>
+    <v-card-text>
+      <editor ref="editor" />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="save"> Save </v-btn>
+    </v-card-actions>
+    <v-snackbar v-model="saved" :timeout="1000">Saved</v-snackbar>
   </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/vue-editor";
+import { defineComponent, ref } from "@vue/composition-api";
+import api from "@/api/crud";
+import { get } from "lodash";
 
 export default defineComponent({
   name: "Privacy",
 
-  setup() {
+  components: {
+    editor: Editor
+  },
+
+  setup(_, { refs }) {
     const title = "Privacy";
+    const industry = ref([]);
+    const saved = ref(false);
+
+    api.get("/Industry/2").then(({ data }) => {
+      industry.value = data;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      refs.editor.invoke("setHtml", get(JSON.parse(industry.value["meta"]), "html"));
+    });
+
+    function save() {
+      let html = this.$refs.editor.invoke("getHtml");
+      let meta = JSON.parse(industry.value["meta"] || "");
+      meta.html = html;
+      industry.value["meta"] = JSON.stringify(meta);
+      api.update("/Industry/2", industry.value);
+      saved.value = true;
+    }
 
     return {
-      title
+      title,
+      save,
+      saved
     };
   }
 });
--- src/views/components/more/terms.vue
+++ src/views/components/more/terms.vue
@@ -3,17 +3,34 @@
     <v-card-title class="primary justify-center display-1 text-h5 white--text">
       {{ title }}
     </v-card-title>
+    <v-card-text>
+      <viewer ref="editor" />
+    </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, onMounted } from "@vue/composition-api";
+import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+import { Viewer } from "@toast-ui/vue-editor";
+import api from "@/api/crud";
+import { get } from "lodash";
 
 export default defineComponent({
   name: "Terms",
-
-  setup() {
+  components: {
+    viewer: Viewer
+  },
+  setup(_, { refs }) {
     const title = "Terms";
+
+    onMounted(() => {
+      api.get("/Industry/2").then(({ data }) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        refs.editor.invoke("setMarkdown", get(JSON.parse(data["meta"]), "html"));
+      });
+    });
 
     return {
       title
