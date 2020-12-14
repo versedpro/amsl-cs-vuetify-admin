ba4eb81474bee1c3aed65cb102f22c57c3390439
Hrachik Simonyan <hrachiksimonyan@gmail.com>
2020-12-14 19:25:45 +0400

    update password change dialog

--- src/views/components/more/password-change-dialog.vue
+++ src/views/components/more/password-change-dialog.vue
@@ -8,6 +8,7 @@
         <v-card-text class="pt-0 mt-0">
           <v-form>
             <v-text-field
+              v-model="model.currentPassword"
               :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
               :label="$t('update_password.oldPassword')"
               :type="showPassword ? 'text' : 'password'"
@@ -17,6 +18,7 @@
               autocomplete="current-password"
             />
             <v-text-field
+              v-model="model.newPassword"
               :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
               :label="$t('update_password.newPassword')"
               :type="showPassword ? 'text' : 'password'"
@@ -38,7 +40,7 @@
         </v-card-text>
         <v-card-actions>
           <v-btn rounded color="primary" text @click="handleCancel">Cancel</v-btn>
-          <v-btn rounded color="primary" text @click="handleCancel">Save</v-btn>
+          <v-btn rounded color="primary" text @click="handleSave">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -46,6 +48,7 @@
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "@vue/composition-api";
+import Axios from "axios";
 
 export default defineComponent({
   name: "Dialog",
@@ -54,13 +57,37 @@ export default defineComponent({
   },
   setup(_, { emit }) {
     const showPassword = ref(false);
+    const model = ref({
+      login: "",
+      currentPassword: "",
+      newPassword: ""
+    });
 
     function handleCancel() {
       emit("on-delete-cancel");
     }
+
+    async function handleSave() {
+      model.value.login = this.$store.getters.user;
+      console.log(model.value);
+      await Axios({
+        url: "/api/StaffAuth/ChangePassword",
+        method: "PUT",
+        data: model.value
+      })
+        .then((response) => {
+          alert("Password changed!");
+          emit("on-delete-cancel");
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
     return {
+      model,
       showPassword,
-      handleCancel
+      handleCancel,
+      handleSave
     };
   }
 });
